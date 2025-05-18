357ee0e442f813876107b6f35e60f6c47b79efea
Attri <Attri.Siddhant@otsuka.jp>
2025-05-18 15:47:57 +0900

    chore: switch Amplify to Next.js SSR hosting

--- src/app/api/jobs/[id]/route.ts
+++ src/app/api/jobs/[id]/route.ts
@@ -62,6 +62,7 @@
 
 
 // src/app/api/jobs/[id]/route.ts// src/app/api/jobs/[id]/route.ts
+export const dynamic = 'force-dynamic'
 
 import { NextResponse } from 'next/server'
 import { db } from '../../../../lib/db'
